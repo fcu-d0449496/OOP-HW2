3f53582a293c245d6e61bbe666d35cf29098ec63
johnn <johnn@DESKTOP-GK454JI>
2016-10-14 11:00:07 +0800

    HW3

--- src/fcu/iecs/oop/StringComparator.java
+++ src/fcu/iecs/oop/StringComparator.java
@@ -7,16 +7,23 @@ public class StringComparator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str1;
-		String str2;  
+		String str2;
+		boolean A=true;
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter a string 1:");
-		str1=scanner.next();
-		System.out.println("Enter a string 2:");
-		str2=scanner.next();
-		if(str1.equalsIgnoreCase(str2))
-			System.out.println("The two strings are the same.");
-		else
+		while(A)
+		{
+			System.out.println("Enter a string 1:");
+			str1=scanner.next();
+			System.out.println("Enter a string 2:");
+			str2=scanner.next();
+			if(str1.equalsIgnoreCase(str2))
+			{
+				System.out.println("The two strings are the same.");
+				break;
+			}
+			else
 			System.out.println("The two strings are not the same.");
+		}
 	}
 
 }
