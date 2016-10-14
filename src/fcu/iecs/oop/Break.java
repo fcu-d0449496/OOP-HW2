10b4d4219579113cb84038187cfe8eaa962d9ed5
johnn <johnn@DESKTOP-GK454JI>
2016-10-14 11:39:26 +0800

    HW3

---
+++ src/fcu/iecs/oop/Break.java
@@ -0,0 +1,16 @@
+package fcu.iecs.oop;
+
+public class Break {
+
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+		int i;
+		for(i=0;i<5;i++)
+		{ 
+			if(i==2)
+				break;
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ src/fcu/iecs/oop/Continue.java
@@ -0,0 +1,15 @@
+package fcu.iecs.oop;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+		int i;
+		for(i = 0; i < 5; i ++) {
+			if(i==0||i==2||i>3)
+				continue;
+			System.out.println(i);
+			}
+	}
+
+}
